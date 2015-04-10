e9d2910e16419641953c0f4e8e1b3f24be49a9f5
mseltzer94 <michael.m.seltzer@gmail.com>
2015-04-10 00:34:24 -0400

    response on sendlocations

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.post('/sendLocation', function(request, response) {
 				response.send(500);
 			}
 			else {
-				response.send(200);
+				response.send(db.collection.find());
 			}
 	    });
 	});
